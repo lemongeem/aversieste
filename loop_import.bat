8ac1a4adc1051e3e94b615a4b687942d1e89c8d3
lemongeem <gabrielin.drz@gmail.com>
2025-08-18 21:46:26 -0600

    Delete loop_import.bat

--- loop_import.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "C:\Users\gabri\Documents\Proyecto tirulacion\base - copia"
-"C:\Users\gabri\AppData\Local\Programs\Python\Python312\python.exe" import_data.py > log.txt 2>&1
-pause
-	
